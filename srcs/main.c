9d1c1b1b8ec37a7119d827b1c9953e5d3501e44e
ArthurCauchy <arthur.cauchy@free.fr>
2018-08-22 09:34:27 +0200

    added main input loop (nothing more yet, exit with ctrl+c)

--- srcs/main.c
+++ srcs/main.c
@@ -6,22 +6,49 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/20 12:03:19 by acauchy           #+#    #+#             */
-/*   Updated: 2018/08/22 09:05:06 by arthur           ###   ########.fr       */
+/*   Updated: 2018/08/22 09:33:31 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdlib.h>
+#include "libft.h"
+#include "utils.h"
 #include "env.h"
 #include "init.h"
 
-int	main(int argc, char **argv, char **envp)
+// TEMPORARY AND UNPROTECTED input loop
+
+static char	*ask_for_input(void)
+{
+	static char	buff[16384];
+	int			size_read;
+	char		*ret;
+
+	ret = NULL;
+	ft_putstr("$> ");
+	size_read = read(0, buff, 16384);
+	if (size_read == -1)
+		exit_error("read() error");
+	buff[size_read] = '\0';
+	if (size_read > 0 && buff[size_read - 1] == '\n')
+		buff[size_read - 1] = '\0';
+	ret = ft_strdup(buff);
+	return (ret);
+}
+
+int			main(int argc, char **argv, char **envp)
 {
 	t_env	*env;
+	char	*input;
 
 	(void)argc;
 	(void)argv;
 	env = NULL;
-	
 	init(&env, envp);
+	while ((input = ask_for_input()) != NULL)
+	{
+		ft_putendl(input);
+	}
 	return (0);
 }
