85980c46303f35387a2300b0cc8be13d2040f522
Ludovic MENTHILLER <lumenthi@e1r11p11.42.fr>
2018-10-16 14:54:56 +0200

    add 1st parsing ?working? version, need to fix leaks

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: lumenthi <lumenthi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/15 15:24:43 by lumenthi          #+#    #+#             */
-/*   Updated: 2018/10/16 12:08:32 by acauchy          ###   ########.fr       */
+/*   Updated: 2018/10/16 14:54:15 by lumenthi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,67 @@
 #include <stdio.h> //for printf, remove after
 #include <stdlib.h>
 
-void word_push(t_word *wordlist, t_word *new_word)
+void display_words(t_word *words)
 {
-	t_word *tmp;
-
-	tmp = wordlist;
+	t_word *tmp = words;
+	ft_putstr("BEGIN DISPLAY\n");
 	while (tmp)
+	{
+		ft_putstr("words: ");
+		ft_putendl(tmp->str);
 		tmp = tmp->next;
-	tmp = (t_word *)malloc(sizeof(t_word));
-	tmp->token = new_word->token;
-	tmp->str = ft_strdup(new_word->str);
-	tmp->next = NULL;
+	}
+	ft_putstr("END DISPLAY\n");
 }
 
-void block_push(t_parse_block *blocklist, t_parse_block *new_block)
+void word_push(t_word **wordlist, t_word *new_word)
 {
-	t_parse_block *tmp;
+	t_word *cur;
+	t_word *prev;
 
-	tmp = blocklist;
-	while (tmp)
-		tmp = tmp->next;
-	tmp = (t_parse_block *)malloc(sizeof(t_parse_block));
-	tmp->wordlist = new_block->wordlist;
-	tmp->separator = new_block->separator;
-	tmp->next = NULL;
+	cur = *wordlist;
+	prev = NULL;
+	while (cur)
+	{
+		prev = cur;
+		cur = cur->next;
+	}
+	cur = (t_word *)malloc(sizeof(t_word));
+	cur->token = new_word->token;
+	cur->str = ft_strdup(new_word->str);
+	cur->next = NULL;
+	if (prev)
+		prev->next = cur;
+	else
+		*wordlist = cur;
+}
+
+void block_push(t_parse_block **blocklist, t_parse_block *new_block)
+{
+	t_parse_block *cur;
+	t_parse_block *prev;
+
+	cur = *blocklist;
+	prev = NULL;
+	while (cur)
+	{
+		prev = cur;
+		cur = cur->next;
+	}
+	cur = (t_parse_block *)malloc(sizeof(t_parse_block));
+	cur->wordlist = new_block->wordlist;
+	cur->separator = new_block->separator;
+	cur->next = NULL;
+	if (prev)
+		prev->next = cur;
+	else
+		*blocklist = cur;
 }
 
 int	is_parsing_separator(t_token elem)
 {
-	if (elem == PIPE || elem == OR || elem == AND || elem == SEMICOL)
+	if (elem == PIPE || elem == OR || elem == AND || elem == SEMICOL ||
+		elem == NONE)
 		return (1);
 	return (0);
 }
@@ -59,16 +91,30 @@ t_parse_block* do_parsing(t_word *wordlist, char **errmsg)
 	tmp_word = NULL;
 	tmp_block = NULL;
 	tmp = wordlist;
+	tmp_block = (t_parse_block *)malloc(sizeof(t_parse_block));
+	tmp_block->wordlist = NULL;
+	tmp_block->next = NULL;
 	(void)errmsg;
 	while (tmp)
 	{
 		if (is_parsing_separator(tmp->token))
 		{
 			tmp_block->separator = tmp->token;
-			block_push(parsing, tmp_block);
+			block_push(&parsing, tmp_block);
 		}
-		word_push(tmp_block->wordlist, tmp);
+		else
+			word_push(&tmp_block->wordlist, tmp);
 		tmp = tmp->next;
 	}
+	//	PRINT
+		// while (tmp_block)
+		// {
+		// 	display_words(tmp_block->wordlist);
+		// 	if (tmp_block->separator == PIPE)
+		// 		printf("separator: PIPE\n");
+		//  	tmp_block = tmp_block->next;
+		// }
+	//	PRINT
+	free(tmp_block);
 	return (parsing);
 }
