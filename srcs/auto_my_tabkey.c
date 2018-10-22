cbd9f8d6bcfdf5383edea3d3a44b0d824abae4cc
Saiqiong XIAO <saxiao@e1r12p12.42.fr>
2018-10-22 20:24:22 +0200

    update

--- srcs/auto_my_tabkey.c
+++ srcs/auto_my_tabkey.c
@@ -6,7 +6,7 @@
 /*   By: saxiao <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/17 15:05:59 by saxiao            #+#    #+#             */
-/*   Updated: 2018/10/22 12:29:33 by saxiao           ###   ########.fr       */
+/*   Updated: 2018/10/22 20:23:38 by saxiao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 #include <unistd.h>
 #include "ft_printf.h"
 #include "line_edit.h"
+#include "global.h"
 
 static void	cusor_back(t_line *line)
 {
@@ -44,11 +45,33 @@ static void	print_autolist(t_line *line)
 	line->auto_ct = line->auto_ct + 1;
 }
 
+static void	home_auto_compare(t_line *line)
+{
+	char	*user_home;
+	char	temp[INPUT_MAX_LEN];
+
+	user_home = NULL;
+	ft_bzero(temp, INPUT_MAX_LEN);
+	if (line->auto_compare[0] == '~')
+	{
+		user_home = read_from_env(&g_env, "HOME");
+		if (user_home)
+		{
+		ft_strcpy((char *)temp, user_home);
+		ft_strcat(temp, (char *)line->auto_compare + 1);
+		ft_bzero(line->auto_compare, INPUT_MAX_LEN);
+		ft_strcpy((char *)line->auto_compare, (char *)temp);
+		free(user_home);
+		}
+	}
+}
+
 int			my_tabkey(t_line *line, t_env **env)
 {
 	ft_bzero((char *)line->auto_compare, INPUT_MAX_LEN);
 	ft_strcpy((char *)line->auto_compare, (char *)line->buf + \
 			auto_start((char *)line->buf));
+	home_auto_compare(line);
 	if (line->auto_ct == -1)
 	{
 		free_auto_lt(line);
