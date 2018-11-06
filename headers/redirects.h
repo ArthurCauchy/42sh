677cee6a3bdc376cb80602e03af035b80b5ba955
Arthur CAUCHY <acauchy@e1r11p11.42.fr>
2018-11-06 16:50:23 +0100

    updated header norm

--- headers/redirects.h
+++ headers/redirects.h
@@ -6,7 +6,7 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 14:11:13 by acauchy           #+#    #+#             */
-/*   Updated: 2018/10/21 12:50:39 by arthur           ###   ########.fr       */
+/*   Updated: 2018/11/06 16:46:54 by acauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@
 
 typedef struct		s_redirect
 {
-	char							*left;
-	char							*right;
-	t_token						token;
+	char				*left;
+	char				*right;
+	t_token				token;
 	struct s_redirect	*next;
 }					t_redirect;
 
@@ -34,15 +34,17 @@ void				delete_redirects(t_redirect *redirs);
 ** redirections.c
 */
 
-void				add_redirect(t_redirect **redirs, char *left, char *right, t_token token);
-int					analyze_redirects(t_word **arglist, t_redirect **redirs, char **errmsg);
+void				add_redirect(t_redirect **redirs,
+		char *left, char *right, t_token token);
+int					analyze_redirects(t_word **arglist,
+		t_redirect **redirs, char **errmsg);
 
 /*
 ** redirections_fdsave.c
 */
 
-void		save_filedes(int *fdtmp_array, int *fdsave_array, int fd);
-void		restore_filedes(int *fdtmp_array, int *fdsave_array);
+void				save_filedes(int *fdtmp_array, int *fdsave_array, int fd);
+void				restore_filedes(int *fdtmp_array, int *fdsave_array);
 
 /*
 ** redirections_apply.c, redirections_apply_[token].c
@@ -54,17 +56,17 @@ int					apply_redirects(t_redirect *redirs,
 		int *fdtmp_array, int *fdsave_array, char **errmsg);
 int					apply_redirect_pipe(t_redirect *redir,
 		int *fdtmp_array, int *fdsave_array, char **errmsg);
-int			apply_redirect_lshift(t_redirect *redir,
+int					apply_redirect_lshift(t_redirect *redir,
 		int *fdtmp_array, int *fdsave_array, char **errmsg);
-int			apply_redirect_lshift_amp(t_redirect *redir,
+int					apply_redirect_lshift_amp(t_redirect *redir,
 		int *fdtmp_array, int *fdsave_array, char **errmsg);
-int			apply_redirect_lshift2(t_redirect *redir,
+int					apply_redirect_lshift2(t_redirect *redir,
 		int *fdtmp_array, int *fdsave_array, char **errmsg);
-int			apply_redirect_rshift(t_redirect *redir,
+int					apply_redirect_rshift(t_redirect *redir,
 		int *fdtmp_array, int *fdsave_array, char **errmsg);
-int			apply_redirect_rshift_amp(t_redirect *redir,
+int					apply_redirect_rshift_amp(t_redirect *redir,
 		int *fdtmp_array, int *fdsave_array, char **errmsg);
-int			apply_redirect_rshift2(t_redirect *redir,
+int					apply_redirect_rshift2(t_redirect *redir,
 		int *fdtmp_array, int *fdsave_array, char **errmsg);
 
 #endif
--- headers/starter.h
+++ headers/starter.h
@@ -6,7 +6,7 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 14:11:13 by acauchy           #+#    #+#             */
-/*   Updated: 2018/11/01 14:25:47 by arthur           ###   ########.fr       */
+/*   Updated: 2018/11/06 16:48:10 by acauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ void				delete_process(t_process *proc);
 ** starter.c
 */
 
-int					start_process(t_env **cmd_env, t_process *proc, int forked, pid_t *pgid);
+int					start_process(t_env **cmd_env,
+		t_process *proc, int forked, pid_t *pgid);
 
 #endif
