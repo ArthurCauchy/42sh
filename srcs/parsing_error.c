22f68b0ad97792ea785fab4636e1c2b7a31788fa
lumenthi <lumenthi@student.42.fr>
2018-11-07 10:46:26 +0100

    norm parsing_error.c

--- srcs/parsing_error.c
+++ srcs/parsing_error.c
@@ -6,7 +6,7 @@
 /*   By: lumenthi <lumenthi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 14:13:44 by lumenthi          #+#    #+#             */
-/*   Updated: 2018/11/06 14:27:11 by lumenthi         ###   ########.fr       */
+/*   Updated: 2018/11/07 10:42:47 by lumenthi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,29 +40,36 @@ static int	parse_error(t_token token)
 	return (0);
 }
 
-int			set_error(t_error *error, t_word *tmp)
+static int	no_error(t_word **tmp, t_error **error)
 {
-	if (is_parsing_arg(tmp->token))
+	if (is_parsing_arg((*tmp)->token))
 	{
-		if (is_parsing_separator(error->separator))
+		if (is_parsing_separator((*error)->separator))
 		{
-			if (error->before == 0)
-				return (parse_error(error->separator));
-			error->after = 1;
-			*error = init_error();
-			error->before = 1;
+			if ((*error)->before == 0)
+				return (parse_error((*error)->separator));
+			(*error)->after = 1;
+			**error = init_error();
+			(*error)->before = 1;
 		}
 		else
-			error->before = 1;
+			(*error)->before = 1;
 	}
-	else if (is_parsing_separator(tmp->token))
+	else if (is_parsing_separator((*tmp)->token))
 	{
-		if (is_parsing_separator(error->separator))
-			return (parse_error(tmp->token));
-		if (error->before == 0)
-			return (parse_error(tmp->token));
-		error->separator = tmp->token;
+		if (is_parsing_separator((*error)->separator))
+			return (parse_error((*tmp)->token));
+		if ((*error)->before == 0)
+			return (parse_error((*tmp)->token));
+		(*error)->separator = (*tmp)->token;
 	}
+	return (1);
+}
+
+int			set_error(t_error *error, t_word *tmp)
+{
+	if (!no_error(&tmp, &error))
+		return (0);
 	if (is_last(tmp))
 	{
 		if (is_parsing_separator(error->separator) && error->separator != SEMICOL)
