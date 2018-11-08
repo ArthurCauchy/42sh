3336a4161a99b67b8a52c1883ffe08e786751177
ArthurCauchy <arthur.cauchy@free.fr>
2018-11-08 23:58:15 +0100

    notes.txt

--- srcs/lexing_escape.c
+++ srcs/lexing_escape.c
@@ -6,7 +6,7 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/04 14:15:05 by acauchy           #+#    #+#             */
-/*   Updated: 2018/09/18 15:52:49 by acauchy          ###   ########.fr       */
+/*   Updated: 2018/11/08 23:38:50 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	lex_escape(char *cmdline, t_lexdata *lexdata)
 {
 	char	next_char;
 
+	lexdata->force_add = 1;
 	next_char = cmdline[lexdata->i + 1];
 	if (next_char)
 	{
