a2264c8d4ac187e9d11496b0c1f3818b018546d2
Camille CHARRIER <ccharrie@e3r3p4.42.fr>
2018-11-09 16:25:36 +0100

    heredox, wip

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: ccharrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/15 15:32:52 by ccharrie          #+#    #+#             */
-/*   Updated: 2018/11/09 15:12:53 by ccharrie         ###   ########.fr       */
+/*   Updated: 2018/11/09 15:57:00 by ccharrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,38 @@
 #include "env.h"
 #include "builtins.h"
 
-void	init_heredoc(t_line *line, t_env *env, char *eof)
+/*static char 	open_heredoc_file(int *fd)
+{
+	if ((fd = open(".heredoc", O_WRONLY | O_CREAT | O_TRUNC)) < 0)
+		return (".heredoc");
+	else if ((fd = open("/tmp/.heredoc", O_WRONLY | O_CREAT | O_TRUNC)) < 0)
+		return ("/tmp/.heredoc");
+//	function to get HOME env variable
+	else if ((fd = open("HOME/.heredoc", O_WRONLY | O_CREAT | O_TRUNC)) < 0)
+		return ("HOME/.heredoc");
+	else
+		return (NULL);
+}*/
+
+int				init_heredoc(int fd, char delim)
 {
 	char	*new_line;
 	char	*heredoc_line;
-	int		fd;
 	int		save_in;
 	
 	new_line = NULL;
 	heredoc_line = NULL;
-	fd = open(".heredoc", O_WRONLY | O_CREAT | O_TRUNC);
 	while (get_line("heredoc> ", new_line, line, env))
 	{
-		if (ft_strcmp(new_line, eof) == 0)
+		if (ft_strcmp(new_line, delim) == 0)
+			break ;
+		else if (/* getting ctrl-d */)
 			break ;
 		heredoc_line = ft_strjoin_free(heredoc_line, new_line);
-		heredoc_line = ft_strjoin_free(heredoc_line, "\n");
+		heredoc_line = ft_strjoin_free(heredoc_line, ft_strdup("\n"));
 	}
-	ft_putstr_fd(heredoc_line, fd);
-//	execute_command(t_line *line);
+	if (heredoc_line)
+		ft_putstr_fd(heredoc_line, fd);
+	else
+		return (-1);
 }
